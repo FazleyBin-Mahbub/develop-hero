99fbe53d70132c5e85881f36d24c35f59b14ce61
Rabby-Cyber <ariyanrabby94@gmail.com>
2021-09-26 22:59:56 +0600

    bug fixed

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -1,21 +1,19 @@
-import React from 'react';
+import React from "react";
 
-import './Cart.css'
+import "./Cart.css";
 const Cart = (props) => {
-    const { team } = props;
-    let total = 0;
-    for (const teams of team) {
-        total = total + teams.salary;
-    }
+  const { team } = props;
+  let total = 0;
+  for (const teams of team) {
+    total = total + teams.salary;
+  }
 
-    return (
-        <div className="cart">
-            <h3> Developer Hired : {props.team.length}</h3>
-            <h3>Total Cost : ${total} </h3>
-
-
-        </div>
-    );
+  return (
+    <div className="cart">
+      <h3> Developer Hired : {props.team.length}</h3>
+      <h3>Total Cost : ${total} </h3>
+    </div>
+  );
 };
 
 export default Cart;
